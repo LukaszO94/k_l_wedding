2f19b97c7e8b8884fd8ffb9e231078c9a76bbfc5
Lukasz_Ochnio <ochnio.lukasz@gmail.com>
2022-09-14 19:58:21 +0200

    Create skeleton and basic func

---
+++ javascript.js
@@ -0,0 +1,35 @@
+function myFunction1() {
+    var x = document.getElementById("schedule");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction2() {
+    var x = document.getElementById("tables");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction3() {
+    var x = document.getElementById("location");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction4() {
+    var x = document.getElementById("contacts");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
+  }
