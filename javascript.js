3913826d321b715a60defb4a6d8a53692506b06e
Lukasz_Ochnio <ochnio.lukasz@gmail.com>
2022-09-19 18:40:03 +0200

    New template v2

--- javascript.js
+++ javascript.js
@@ -1,35 +1,40 @@
 function myFunction1() {
-    var x = document.getElementById("schedule");
-    if (x.style.display === "none") {
-      x.style.display = "flex";
-    } else {
+    const x = document.getElementById("schedule");
+    const y = document.getElementById("Tables");
+    const z = document.getElementById("Location");
+    const n = document.getElementById("Contacts");
+    if (x.style.display === "flex") {
       x.style.display = "none";
-    }
+
+    } else {
+      x.style.display = "flex";
+    };
+    
   }
 
   function myFunction2() {
     var x = document.getElementById("tables");
-    if (x.style.display === "none") {
-      x.style.display = "flex";
-    } else {
+    if (x.style.display === "flex") {
       x.style.display = "none";
+    } else {
+      x.style.display = "flex";
     }
   }
 
   function myFunction3() {
     var x = document.getElementById("location");
-    if (x.style.display === "none") {
-      x.style.display = "flex";
-    } else {
+    if (x.style.display === "flex") {
       x.style.display = "none";
+    } else {
+      x.style.display = "flex";
     }
   }
 
   function myFunction4() {
     var x = document.getElementById("contacts");
-    if (x.style.display === "none") {
-      x.style.display = "flex";
-    } else {
+    if (x.style.display === "flex") {
       x.style.display = "none";
+    } else {
+      x.style.display = "flex";
     }
   }
